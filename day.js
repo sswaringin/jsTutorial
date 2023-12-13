7e2c49794c8e5f4db724468f45b39ba617cf3ffd
Stephen <stephen.swaringin@gmail.com>
2023-12-13 06:54:55 -0600

    Moved dayName function to day.js file

---
+++ day.js
@@ -0,0 +1,9 @@
+// Returns the day of the week for the given date.
+function dayName(date) {
+    const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return daysOfTheWeek[date.getDay()];
+}
+
+function greeting(date) {
+    return `Hello, world! Happy ${dayName(date)}!`;
+}
